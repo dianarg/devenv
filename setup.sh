f68974b82c6b28aa34e0a5194a924f51eb70e4a4
Diana Guttman <diana.guttman@gmail.com>
2020-07-23 11:13:57 -0700

    Add commandline fuzzy finder
    Signed-off-by: Diana Guttman <diana.guttman@gmail.com>

--- setup.sh
+++ setup.sh
@@ -8,3 +8,9 @@ cp --backup=numbered .gitconfig $HOME/
 cp --backup=numbered .tmux.conf $HOME/
 mkdir -p $HOME/.config
 cp --backup=numbered pep8 $HOME/.config/pep8
+
+read -p "Install commandline fuzzy finder? [Yy/Nn]" yn
+case $yn in
+    [Yy]* ) git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install; break;;
+    * ) exit;;
+esac
